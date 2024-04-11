7bbfdfeb8d6bb165eb6ba4b96d54e1d1a5918da9
MarioContrerasLara <163984658+MarioContrerasLara@users.noreply.github.com>
2024-04-11 09:10:00 +0200

    Add files via upload

---
+++ Web2/Web2/Controllers/ProductoController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using Web2.Contracts;
+using Web2.Repositories;
+using Web2.Models;
+
+namespace Web2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private readonly IProductoRepository _productoRepository;
+
+        public ProductoController(IProductoRepository productoRepository)
+        {
+            _productoRepository = productoRepository;
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Productos nuevoArticulo)
+        {
+            try
+            {
+                _productoRepository.AddProduct(nuevoArticulo);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}/increase")]
+        public IActionResult IncreaseQuantity(int id, int cantidad)
+        {
+            try
+            {
+                _productoRepository.IncreaseQuantity(id, cantidad);
+                return Ok();
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPut("{id}/decrease")]
+        public IActionResult DecreaseQuantity(int id, int cantidad)
+        {
+            try
+            {
+                _productoRepository.DecreaseQuantity(id, cantidad);
+                return Ok();
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (InvalidOperationException)
+            {
+                return BadRequest("No hay suficiente cantidad en el almacen");
+            }
+        }
+    }
+}
---
+++ Web2/Web2/Models/Productos.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Web2.Models
+{
+    public class Productos
+    {
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Descripción { get; set; }
+    public DateAndTime FechaEntrada { get; set; }
+    //public DateFormat FechaEntrada { get; set; }
+    public int Cantidad { get; set; }
+    }
+}
