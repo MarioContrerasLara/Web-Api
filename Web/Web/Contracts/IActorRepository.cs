b53dea1983f0dc0735314f5af732529519b48a56
MarioContrerasLara <163984658+MarioContrerasLara@users.noreply.github.com>
2024-04-12 12:18:52 +0200

    Add files via upload

---
+++ Web/Web/Contracts/IActorRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Web.Models;
+
+namespace Web.Contracts
+{
+    public interface IActorRepository
+    {
+        List<Actor> GetActors();
+        Actor GetActorById(int id);
+        void AddActor(Actor actor);
+        void UpdateActor(int id, Actor actor);
+        void DeleteActor(int id);
+    }
+}
