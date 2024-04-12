bd98e10825efb9dba8a529c1f1f53c96591746d8
MarioContrerasLara <163984658+MarioContrerasLara@users.noreply.github.com>
2024-04-12 12:12:38 +0200

    Delete Web directory

--- Web/Web/Models/Pelicula.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Web.Models
-{
-    public class Pelicula
-    {
-        public int Id { get; set; }
-        public string Titulo { get; set; }
-        public int FechaPublicacion { get; set; }
-    }
-}
